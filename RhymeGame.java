b266faa33f27a98ec395ba087ca82fc24e1b17ec
abhigya-sodani <37461794+abhigya-sodani@users.noreply.github.com>
2019-05-09 08:29:14 -0700

    Create RhymeGame.java

---
+++ RhymeGame.java
@@ -0,0 +1,56 @@
+import org.jsoup.*;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import java.util.*;
+public class RhymeGame{
+	String word;
+	ArrayList<String> rhymes=new ArrayList<String>();
+	public RhymeGame(String w) {
+		word=w;
+	}
+	public void getArray() {
+		try {
+			
+			String url="https://www.rhymezone.com/r/rhyme.cgi?Word="+word+"&typeofrhyme=perfect&org1=syl&org2=l&org3=y";
+			
+			Document doc = Jsoup.connect(url).userAgent("Chrome/74.0.3729.131").get();
+			Elements temp=doc.select("a");
+			for(Element a:temp) {
+				String b=a.text();
+				b=b.replaceAll(" ", "_");
+				
+				
+				if(a.attr("href").equals("d="+b)) {
+					rhymes.add(a.text().toLowerCase().replaceAll(" ","").replaceAll("-","").replaceAll("'", ""));
+					
+				}
+				
+			}
+		System.out.println(rhymes);
+		}
+		catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	public static String getRandomWord() {
+		try {
+			Document doc = Jsoup.connect("https://randomword.com/").userAgent("Chrome/74.0.3729.131").get();
+			Elements temp=doc.select("div");
+			for(Element a:temp) {
+				if(a.attr("id").equals("random_word")) {
+					return a.text();
+				}
+			}
+			return "a";
+		}
+		catch(Exception e) {
+			System.out.println(e.getMessage());
+			return "a";
+		}
+		
+	}
+	
+	
+}
