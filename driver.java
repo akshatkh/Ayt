ca5c0244fdce485437309e141dd3d2dbc9f1176b
abhigya-sodani <37461794+abhigya-sodani@users.noreply.github.com>
2019-05-13 17:57:54 -0700

    Update driver.java

--- driver.java
+++ driver.java
@@ -1,11 +1,24 @@
+import java.util.*;
+import java.io.*;
 public class driver{
 	
 	
 	public static void main(String[] args) {
+		while(true) {
 		String b=RhymeGame.getRandomWord();
 		RhymeGame a=new RhymeGame(b);
 		
 		System.out.println(b);
 		a.getArray();
+		ArrayList rhymes=a.rhymes;
+		Scanner in= new Scanner(System.in);
+		if(rhymes.contains(in.nextLine().toLowerCase().replaceAll(" ","" ).replaceAll("'","").replaceAll("-", ""))) {
+			System.out.println("Thats a rhyme!");
+			
+		}
+		else {
+			System.out.println("Lmao Nope!");
+		}
+	}
 	}
 }
